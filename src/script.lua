dac845a9fe8f6913fcb3edc3af6017b6353cdf20
TyKonKet <matteoautieri@live.it>
2017-09-14 12:21:53 +0200

    description

--- src/script.lua
+++ src/script.lua
@@ -42,35 +42,35 @@ function EasierModding:loadMap(name)
     if not g_currentMission.missionDynamicInfo.isMultiplayer then
         addConsoleCommand("emReloadVehicle", "Reloads a whole vehicle", "consoleCommandReloadVehicle", g_currentMission);
     end
+    addConsoleCommand("emChangeLanguage", "Changes the game language", "consoleCommandChangeLanguage");
+    addConsoleCommand("emReloadCurrentGui", "Reloads the gui", "consoleCommandReloadCurrentGui");
+    addConsoleCommand("emSetDebugRenderingMode", "Changes the rendering mode", "consoleCommandSetDebugRenderingMode");
     addConsoleCommand("emShowVehicleDistance", "Shows the distance between vehicle and cam", "consoleCommandShowVehicleDistance", g_currentMission);
-    addConsoleCommand("emSetDebugRenderingMode", "", "consoleCommandSetDebugRenderingMode");
-    addConsoleCommand("emChangeLanguage", "", "consoleCommandChangeLanguage");
-    addConsoleCommand("emReloadCurrentGui", "", "consoleCommandReloadCurrentGui");
-    addConsoleCommand("emSuspendApp", "", "consoleCommandSuspendApp");
-    addConsoleCommand("emVerifyI18N", "", "consoleCommandVerifyAll", g_i18n);
+    addConsoleCommand("emSuspendApp", "Suspends the game", "consoleCommandSuspendApp");
+    addConsoleCommand("emVerifyI18N", "Checks the I18N", "consoleCommandVerifyAll", g_i18n);
     if g_currentMission:getIsServer() then
-        addConsoleCommand("emCheatMoney", "Add a lot of money", "consoleCommandCheatMoney", g_currentMission);
-        addConsoleCommand("emBuyField", "Buys a field", "consoleCommandBuyField", g_currentMission);
+        addConsoleCommand("emAddBale", "Adds a bale", "consoleCommandAddBale", g_currentMission);
         addConsoleCommand("emBuyAllFields", "Buys all fields", "consoleCommandBuyAllFields", g_currentMission);
+        addConsoleCommand("emBuyField", "Buys a field", "consoleCommandBuyField", g_currentMission);
+        addConsoleCommand("emCheatMoney", "Add a lot of money", "consoleCommandCheatMoney", g_currentMission);
+        addConsoleCommand("emCheatSilo", "Add silo amount", "consoleCommandCheatSilo", g_currentMission);
+        addConsoleCommand("emDeleteAllVehicles", "Deletes all vehicles", "consoleCommandDeleteAllVehicles", g_currentMission);
+        addConsoleCommand("emExportStoreItems", "Exports storeItem data", "consoleCommandExportStoreItems", g_currentMission);
+        addConsoleCommand("emFillVehicle", "Fills the vehicle with given filltype", "consoleCommandFillVehicle", g_currentMission);
+        addConsoleCommand("emSetAnimals", "Sets the amount of given animals", "consoleCommandSetAnimals", self);
+        addConsoleCommand("emSetDirtScale", "Sets a given dirt scale", "consoleCommandSetDirtScale", g_currentMission);
         addConsoleCommand("emSetFieldFruit", "Sets a given fruit to field", "consoleCommandSetFieldFruit", g_currentMission);
         addConsoleCommand("emSetFieldGround", "Sets a given fruit to field", "consoleCommandSetFieldGround", g_currentMission);
-        addConsoleCommand("emShowTipCollisions", "Shows the collisions for tipping on the ground", "consoleCommandShowTipCollisions", g_currentMission);
-        addConsoleCommand("emAddBale", "Adds a bale", "consoleCommandAddBale", g_currentMission);
         addConsoleCommand("emSetFuel", "Sets the vehicle fuel level", "consoleCommandSetFuel", g_currentMission);
-        addConsoleCommand("emFillVehicle", "Fills the vehicle with given filltype", "consoleCommandFillVehicle", g_currentMission);
         addConsoleCommand("emSetOperatingTime", "Sets the vehicle operating time", "consoleCommandSetOperatingTime", g_currentMission);
-        addConsoleCommand("emSetDirtScale", "Sets a given dirt scale", "consoleCommandSetDirtScale", g_currentMission);
-        addConsoleCommand("emCheatSilo", "Add silo amount", "consoleCommandCheatSilo", g_currentMission);
-        addConsoleCommand("emExportStoreItems", "Exports storeItem data", "consoleCommandExportStoreItems", g_currentMission);
+        addConsoleCommand("emShowTipCollisions", "Shows the collisions for tipping on the ground", "consoleCommandShowTipCollisions", g_currentMission);
         addConsoleCommand("emStartBrandSale", "Starts a brand sale", "consoleStartBrandSale", g_currentMission);
-        addConsoleCommand("emStartVehicleSale", "Starts a vehicle sale", "consoleStartVehicleSale", g_currentMission);
         addConsoleCommand("emStartGreatDemand", "Starts a great demand", "consoleStartGreatDemand", g_currentMission);
-        addConsoleCommand("emUpdateTipCollisions", "Updates the collisions for tipping on the ground around the current camera", "consoleCommandUpdateTipCollisions", g_currentMission);
-        addConsoleCommand("emTeleport", "Teleports to given field or x/z-position", "consoleCommandTeleport", g_currentMission);
-        addConsoleCommand("emTakeVehicleScreenshotsFromOutside", "Takes several screenshots of the selected vehicle from outside", "consoleCommandTakeScreenshotsFromOutside", g_currentMission);
+        addConsoleCommand("emStartVehicleSale", "Starts a vehicle sale", "consoleStartVehicleSale", g_currentMission);
         addConsoleCommand("emTakeVehicleScreenshotsFromInside", "Takes several screenshots of the selected vehicle from inside", "consoleCommandTakeScreenshotsFromInside", g_currentMission);
-        addConsoleCommand("emDeleteAllVehicles", "Deletes all vehicles", "consoleCommandDeleteAllVehicles", g_currentMission);
-        addConsoleCommand("emSetAnimals", "Sets the amount of given animals", "consoleCommandSetAnimals", self);
+        addConsoleCommand("emTakeVehicleScreenshotsFromOutside", "Takes several screenshots of the selected vehicle from outside", "consoleCommandTakeScreenshotsFromOutside", g_currentMission);
+        addConsoleCommand("emTeleport", "Teleports to given field or x/z-position", "consoleCommandTeleport", g_currentMission);
+        addConsoleCommand("emUpdateTipCollisions", "Updates the collisions for tipping on the ground around the current camera", "consoleCommandUpdateTipCollisions", g_currentMission);
     end
     self:loadSavegame();
 end
@@ -91,35 +91,35 @@ function EasierModding:saveSavegame()
 end
 
 function EasierModding:deleteMap()
+    removeConsoleCommand("emAddBale");
+    removeConsoleCommand("emBuyAllFields");
+    removeConsoleCommand("emBuyField");
+    removeConsoleCommand("emChangeLanguage");
     removeConsoleCommand("emCheatMoney");
-    removeConsoleCommand("emExportStoreItems");
-    removeConsoleCommand("emStartBrandSale");
-    removeConsoleCommand("emStartVehicleSale");
-    removeConsoleCommand("emStartGreatDemand");
     removeConsoleCommand("emCheatSilo");
+    removeConsoleCommand("emDeleteAllVehicles");
+    removeConsoleCommand("emExportStoreItems");
     removeConsoleCommand("emFillVehicle");
+    removeConsoleCommand("emReloadCurrentGui");
+    removeConsoleCommand("emReloadVehicle");
+    removeConsoleCommand("emSetAnimals");
+    removeConsoleCommand("emSetDebugRenderingMode");
+    removeConsoleCommand("emSetDirtScale");
+    removeConsoleCommand("emSetFieldFruit");
+    removeConsoleCommand("emSetFieldGround");
     removeConsoleCommand("emSetFuel");
     removeConsoleCommand("emSetOperatingTime");
-    removeConsoleCommand("emAddBale");
     removeConsoleCommand("emShowTipCollisions");
-    removeConsoleCommand("emUpdateTipCollisions");
     removeConsoleCommand("emShowVehicleDistance");
-    removeConsoleCommand("emSetDirtScale");
-    removeConsoleCommand("emBuyAllFields");
-    removeConsoleCommand("emSetFieldFruit");
-    removeConsoleCommand("emReloadVehicle");
-    removeConsoleCommand("emTeleport");
-    removeConsoleCommand("emSetAnimals");
-    removeConsoleCommand("emBuyField");
-    removeConsoleCommand("emSetFieldGround");
-    removeConsoleCommand("emTakeVehicleScreenshotsFromOutside");
-    removeConsoleCommand("emTakeVehicleScreenshotsFromInside");
-    removeConsoleCommand("emDeleteAllVehicles");
-    removeConsoleCommand("emVerifyI18N");
-    removeConsoleCommand("emSetDebugRenderingMode");
-    removeConsoleCommand("emChangeLanguage");
-    removeConsoleCommand("emReloadCurrentGui");
+    removeConsoleCommand("emStartBrandSale");
+    removeConsoleCommand("emStartGreatDemand");
+    removeConsoleCommand("emStartVehicleSale");
     removeConsoleCommand("emSuspendApp");
+    removeConsoleCommand("emTakeVehicleScreenshotsFromInside");
+    removeConsoleCommand("emTakeVehicleScreenshotsFromOutside");
+    removeConsoleCommand("emTeleport");
+    removeConsoleCommand("emUpdateTipCollisions");
+    removeConsoleCommand("emVerifyI18N");
 end
 
 function EasierModding:keyEvent(unicode, sym, modifier, isDown)
@@ -135,7 +135,7 @@ function EasierModding:draw()
 end
 
 function EasierModding:consoleCommandSetAnimals(type, amount)
-    local usage = "emSetAnimals type[cow | pig | sheep] amount";
+    local usage = "emSetAnimals type(cow | pig | sheep) amount";
     local husbandry = g_currentMission.husbandries[type];
     if type == nil or type == "" or husbandry == nil then
         return "Invalid type. " .. usage;
